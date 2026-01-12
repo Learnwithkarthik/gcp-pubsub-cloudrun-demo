2fb602d3d783a80494fa6687db880c743d64039c
Go with GCP <141059279+Learnwithkarthik@users.noreply.github.com>
2026-01-12 20:31:15 +0530

    Update app.py

--- subscriber/app.py
+++ subscriber/app.py
@@ -13,22 +13,10 @@ def health():
 
 # POST – Pub/Sub push endpoint
 @app.route("/", methods=["POST"])
+@app.route("/", methods=["POST"])
 def receive_message():
-    envelope = request.get_json()
-
-    if not envelope or "message" not in envelope:
-        logging.error("Invalid Pub/Sub message")
-        return ("Bad Request", 400)
-
-    message_data = base64.b64decode(
-        envelope["message"]["data"]
-    ).decode("utf-8")
-
-    data = json.loads(message_data)
-
-    logging.info(f"[SUBSCRIBER-A] Received: {data}")
-
-    return ("OK", 200)
+    logging.error("[SUBSCRIBER-A] Simulated failure")
+    return ("Subscriber-A is down", 500)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
